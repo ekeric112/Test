75cf8295ee88d975b6125f68e8bd3386835e30e8
Eric <ekeric112@gmail.com>
2019-11-06 13:38:39 -0800

    Added a method

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void firstMethod();
+
 int main() {
     cout << "Hello WOrld" << endl;
     cout << "Sike! Now I changed the file on CLion!\n";
+    firstMethod();
     return 0;
+}
+
+void firstMethod(){
+    cout << "Now I just have a function!\n";
 }
